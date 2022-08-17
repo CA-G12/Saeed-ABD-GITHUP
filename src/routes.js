35e3fa488124cbc1c847bfaf0bf4e9609d6f301a
Saeed99Madi <saed.zooro@gmail.com>
2022-08-17 15:35:44 +0300

    write route for search from server related to #19

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
-const user = require('./controllers/user');
-const router = require('express').Router();
+const user = require("./controllers/user");
+const router = require("express").Router();
 
-router.get('/users/:name',user.user)
-router.get('/users/:name/repos',user.repos)
+router.get("/users/:name", user.user);
+router.get("/users/:name/repos", user.repos);
+router.get("/users/:search", user.search);
 
 module.exports = router;
