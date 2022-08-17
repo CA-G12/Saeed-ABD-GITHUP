b8d1926872980d7b12d72f54b40da3e609e5f619
abdou059 <abdoumonuir9@gmail.com>
2022-08-17 15:01:19 +0300

    build fetch

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const user = require('./controllers/user');
 const router = require('express').Router();
 
-router.get('/users/:name',user)
+router.get('/users/:name',user.user)
+router.get('/users/:name/repos',user.repos)
 
 module.exports = router;
